f1b5f383eab879a7cf8c2018ef806481aebc088c
Dan Tench <dan.tench@gmail.com>
2019-08-11 15:31:29 +0100

    init

--- app/mailers/email_processor.rb
+++ app/mailers/email_processor.rb
@@ -11,7 +11,7 @@ class EmailProcessor
     puts "Emails arrived"
     puts "From email: " + @email.from[:email]
     puts "From name: " + @email.from[:name]
-    puts "Header: " + @email.headers[:Date]
+    puts "Header: " + @email.headers[0][:Date]
 
     # here's an example of model creation
     # user = User.find_by_email(@email.from[:email])
