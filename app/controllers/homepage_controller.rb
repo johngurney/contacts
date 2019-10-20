34f16b7a9a402f35968fd34056d17200afb5a171
Dan Tench <dan.tench@gmail.com>
2019-10-20 18:57:59 +0100

    init

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -76,7 +76,7 @@ class HomepageController < ApplicationController
     user_id= cookies[:location_user_id].to_i
     @user = User.find(user_id) if user_id > 0 && User.where(:id == user_id).count > 0
 
-    render "location" #, :layout => "location"
+    render "test" #{}"location" #, :layout => "location"
 
   end
 
